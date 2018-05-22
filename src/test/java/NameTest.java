d680e31b279da52ecc51643e9021076d2a5b389e
stooggle <stuartodonnell@gmail.com>
2018-05-22 09:57:04 +0100

    Adds NameTest

--- src/test/java/NameTest.java
+++ src/test/java/NameTest.java
@@ -1,5 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class NameTest {
 
-
-
+    @Test
+    public void hasListOfStrings() {
+        Name name = new Name();
+        assertEquals(21, name.getNames().size());
+    }
 }
