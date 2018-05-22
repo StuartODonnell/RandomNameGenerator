683b0955533e562ba6f2b3e614977bc8b19fda6e
stooggle <stuartodonnell@gmail.com>
2018-05-22 08:13:55 +0100

    Initial commit. Adds single name returning.

---
+++ src/main/java/Name.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static spark.Spark.get;
+
+public class Name {
+
+    private ArrayList<String> names;
+
+    public Name() {
+        this.names = new ArrayList<>();
+        names.add("The Hound");
+        names.add("The Mountain");
+        names.add("Victarion");
+        names.add("Mance Rayder");
+        names.add("Hot Pie");
+        names.add("Areo Hotah");
+        names.add("Khal Jhaqo");
+        names.add("Moqorro The Red Priest");
+        names.add("Reek");
+        names.add("Ramsay Bolton");
+        names.add("Ser Arys Oakheart");
+        names.add("Lady Stoneheart");
+        names.add("Brienne of Tarth");
+        names.add("Ser Davos Seaworth");
+        names.add("Strong Belwas");
+        names.add("Ser Barristan Selmy");
+        names.add("Willem Darry");
+        names.add("Jon Connington");
+        names.add("Young Griff");
+        names.add("Rhllor");
+        names.add("Hodor");
+
+    }
+
+    public ArrayList<String> getNames() {
+        return names;
+    }
+
+    public void setNames(ArrayList<String> names) {
+        this.names = names;
+    }
+
+    public String returnName(){
+        Collections.shuffle(names);
+            return names.get(0);
+
+    }
+
+//    public String returnNamePair(){
+//        Collections.shuffle(names);
+//        return names.get(0), names.get(1);
+//    }
+
+
+
+    }
+
---
+++ src/main/java/NameController.java
@@ -0,0 +1,34 @@
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static spark.Spark.get;
+import static spark.SparkBase.staticFileLocation;
+
+public class NameController {
+
+
+    public static void main(String[] args) {
+        VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
+        staticFileLocation("/public");
+
+        Name name = new Name();
+
+        get("/one", (req, res) -> {
+            String result = name.returnName();
+            HashMap<String, Object> model = new HashMap<>();
+            model.put("name", result);
+            return new ModelAndView(model, "name.vtl");
+        }, velocityTemplateEngine);
+
+//        get("/two", (req, res) -> {
+//            String result = name.returnNamePair();
+//            HashMap<String, Object> model = new HashMap<>();
+//            model.put("name_pair", result);
+//            return new ModelAndView(model, "name_pair.vtl");
+//        }, velocityTemplateEngine);
+    }
+
+}
